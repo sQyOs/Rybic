3734c99a8cda2b0059bff385fd781adb1a8978ed
sQyOs <onigagazol@gmail.com>
2020-09-02 12:43:23 +1000

    Modify control manager for side faces

--- Assets/Scripts/ControlManager.cs
+++ Assets/Scripts/ControlManager.cs
@@ -11,7 +11,10 @@ public class ControlManager : MonoBehaviour
     private Vector3 currentMousePosition = Vector3.zero;
     private float mouseMoveAngle = 0;
     private float cameraRelativeAngle = 0;
+    private string axisForRotation = "";
+    private float _sign;
     private Transform cameraTransform;
+    private Transform _selectedUnit;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,8 @@ public class ControlManager : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            _selectedUnit = controller.GetComponent<SelectionManager>().selection;
+
             //вычисляем текущие координаты курсора
             currentMousePosition = Input.mousePosition;
 
@@ -40,39 +45,90 @@ public class ControlManager : MonoBehaviour
             {
                 //вычисляем угол поворота камеры по оси Y относительно куба с поправкой на 45 градусов, в секторах по 180, в радианах
                 cameraRelativeAngle = ((cameraTransform.eulerAngles.y + 45) % 180) * Mathf.Deg2Rad;
-
-                //вычисляем вектор(ось матрица поворота) для определения угла движения указателя с поправкой на поворот камеры относительно куба
-                //и вектор перпендикуляр для определения знака (верх, низ)
-                Vector3 axisCorrection = new Vector3(Mathf.Sin(cameraRelativeAngle), -Mathf.Cos(cameraRelativeAngle));
-                Vector3 axisCorrectionPerpend = new Vector3(-axisCorrection.y / axisCorrection.x, 1);
-
-                //вычисляем через скалярное произведение знаки движения курсора и расположения камеы(инвертированно под левостороннюю систему юнити)
-                float signMouseMove = Mathf.Sign(Vector3.Dot(axisCorrectionPerpend, currentMousePosition - startMousePosition));
                 float signCameraPosition = -Mathf.Sign(Vector3.Dot(new Vector3(1, 0, 1) * targetCenter, cameraTransform.position - Vector3.one * targetCenter));
 
-                //вычисляем и выводим угол движения указателя и предполагаемое вращение куба по осям относительно камеры
-                //[90, 180] +X, [0, 90] +Z, [-90, 0] -X, [-180, -90] -Z 
-                mouseMoveAngle = signMouseMove * Vector3.Angle(axisCorrection, currentMousePosition - startMousePosition);
-                if (mouseMoveAngle > 90)
+                if ((_selectedUnit.eulerAngles.normalized.x + _selectedUnit.eulerAngles.normalized.y + _selectedUnit.eulerAngles.normalized.z) != 1)
                 {
-                    Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"X"} || direction: {signCameraPosition * signMouseMove} ");
-                }
-                else if (mouseMoveAngle > 0)
-                {
-                    Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"Z"} || direction: {signCameraPosition * signMouseMove} ");
-                }
-                else if (mouseMoveAngle > -90)
-                {
-                    Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"X"} || direction: {signCameraPosition * signMouseMove} ");
+                    //вычисляем вектор(ось матрица поворота) для определения угла движения указателя с поправкой на поворот камеры относительно куба
+                    //и вектор перпендикуляр для определения знака (верх, низ)
+                    Vector3 axisCorrection = new Vector3(Mathf.Sin(cameraRelativeAngle), -Mathf.Cos(cameraRelativeAngle));
+                    Vector3 axisCorrectionPerpend = new Vector3(-axisCorrection.y / axisCorrection.x, 1);
+
+                    //вычисляем через скалярное произведение знаки движения курсора и расположения камеы(инвертированно под левостороннюю систему юнити)
+                    float signMouseMove = Mathf.Sign(Vector3.Dot(axisCorrectionPerpend, currentMousePosition - startMousePosition));
+                    _sign = signCameraPosition * signMouseMove;
+
+                    //вычисляем и выводим угол движения указателя и предполагаемое вращение куба по осям относительно камеры
+                    //[90, 180] or [-90, 0] : X, [0, 90] or [-180, -90] : Z 
+                    mouseMoveAngle = signMouseMove * Vector3.Angle(axisCorrection, currentMousePosition - startMousePosition);
+                    if (mouseMoveAngle > 90 || (mouseMoveAngle < 0 && mouseMoveAngle > -90))
+                    {
+                        axisForRotation = "X";
+                    }
+                    else
+                    {
+                        axisForRotation = "Z";
+                    }
                 }
                 else
                 {
-                    Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"Z"} || direction: {signCameraPosition * signMouseMove} ");
+                    float signMouseMove = Mathf.Sign(Vector3.Dot(new Vector3(1, 1), currentMousePosition - startMousePosition));
+                    _sign = signCameraPosition * signMouseMove;
+                    mouseMoveAngle = signMouseMove * Vector3.Angle(new Vector3(1, -1), currentMousePosition - startMousePosition);
+                    if (mouseMoveAngle > 90 || (mouseMoveAngle < 0 && mouseMoveAngle > -90))
+                    {
+                        if (cameraRelativeAngle < 1.57f)
+                        {
+                            axisForRotation = "X";
+                        }
+                        else
+                        {
+                            axisForRotation = "Z";
+                        }
+                    }
+                    else
+                    {
+                        axisForRotation = "Y";
+                        _sign = signMouseMove;
+                    }
                 }
-
+                Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {axisForRotation} || direction: {_sign} ");
             }
 
         }
 
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        if (_selectedUnit != null)
+        {
+            Gizmos.DrawWireCube(_selectedUnit.position, Vector3.one);
+            //if (Vector3.Distance(startMousePosition, currentMousePosition) > positionDifference)
+            //{
+            //    Gizmos.color = Color.magenta;
+            //    float dimension = targetCenter + 0.5f * 2;
+            //    if (mouseMoveAngle > 90)//+X vector forward
+            //    {
+            //        Gizmos.DrawWireCube(Vector3.one * targetCenter + Vector3.Cross(new Vector3(1, 0, 0), _selectedUnit.position) / 2, Vector3.one);
+            //        Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"X"} || direction: {_sign} ");
+            //    }
+            //    else if (mouseMoveAngle > 0)//+Z vector right
+            //    {
+            //        Gizmos.DrawWireCube(Vector3.one * targetCenter + Vector3.Cross(new Vector3(0, 0, 1), _selectedUnit.position) / 2, Vector3.one);
+            //        Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"Z"} || direction: {_sign} ");
+            //    }
+            //    else if (mouseMoveAngle > -90)//-X
+            //    {
+            //        Gizmos.DrawWireCube(Vector3.one * targetCenter + Vector3.Cross(new Vector3(1, 0, 0), _selectedUnit.position) / 2, Vector3.one);
+            //        Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"X"} || direction: {_sign} ");
+            //    }
+            //    else//-Z
+            //    {
+            //        Gizmos.DrawWireCube(Vector3.one * targetCenter + Vector3.Cross(new Vector3(0, 0, 1), _selectedUnit.position) / 2, Vector3.one);
+            //        Debug.Log($"cameraAngle: {cameraRelativeAngle * Mathf.Rad2Deg} || mouse angle: {mouseMoveAngle} || axis: {"Z"} || direction: {_sign} ");
+            //    } 
+            //}
+        }
     }
 }
